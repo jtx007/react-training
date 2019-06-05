2d0410e9c362b4e532bebf774b73aace23b8b630
jtx007 <jamesjacobthomas7@gmail.com>
2019-06-05 16:24:35 -0400

    upgraded handlebars

--- test/src/components/test.js
+++ test/src/components/test.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 export default class Address extends Component {
     render() {
         return (
             <div>
-                
+                My Address is {this.props.address}
             </div>
         )
     }
+}
+
+Address.propTypes = {
+    address: PropTypes.string
 }
