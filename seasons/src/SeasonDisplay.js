956d2ca8ae29514c69c1c45d8d00e86ed6a94102
jtx007 <jamesjacobthomas7@gmail.com>
2019-06-11 17:52:15 -0400

    seasons mini app MVP complete

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import './SeasonDisplay.css'
+const SeasonDisplay = (props) => {
 
-const SeasonDisplay = () => {
-    return (
-        <div>Season Display</div>
-    )
+    const seasonConfig = {
+        winter: {
+            iconName: 'snowflake',
+            text: 'BRRR, it chilly'
+        },
+        summer: {
+            iconName: 'sun',
+            text: 'TO the beach!'
+        }
+    }
+
+    const { latitude } = props
+    const getSeason = (lat, month) => {
+        if (month > 2 && month < 9) {
+            return lat > 0 ? 'summer' : 'winter'
+        } else {
+            return lat < 0 ? 'winter' : 'summer'
+        }
+    }
+
+    const season = getSeason(latitude, new Date().getMonth())
+    const { iconName, text} = seasonConfig[season]
+    console.log(props)
+    return <div className={`season-display ${season}`}>
+        <i className={`icon-left ${iconName} icon massive`} />
+            <h1>{text}</h1>
+        <i className={`icon-right ${iconName} icon massive`} />
+    </div>
+
 }
 
 export default SeasonDisplay
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,33 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay'
-
+import Loader from './Loader'
 
 
 export default class App extends React.Component {
-    constructor(props) {
-        super(props);
+    state = {
+        lat: null,
+        errorMessage: ''
+    }
 
-        this.state = {
-            lat: null,
-            errorMessage: ''
-        }
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 this.setState({
                     lat: position.coords.latitude
                 })
             },
-            error => { 
-                this.setState({ 
+            error => {
+                this.setState({
                     errorMessage: error.message
-                }) 
-                }
+                })
+            }
         )
-    }
-
-    componentDidMount() {
-        console.log('My component is here')
     }
 
     componentDidUpdate() {
@@ -35,16 +30,15 @@ export default class App extends React.Component {
     }
     
     render() {
-        
             if (this.state.errorMessage && !this.state.lat) {
                 return <div>Error: {this.state.errorMessage}</div>
             }
 
             if (!this.state.errorMessage && this.state.lat) {
-                return <div>Latitude: {this.state.lat}</div>
+                return <SeasonDisplay latitude={this.state.lat} />
             }
 
-            return <div>Loading...</div>
+            return <Loader />
         
     }
 }
