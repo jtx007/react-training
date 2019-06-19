97547cab721c4d7d31e9208af78e38386b7da0c4
jtx007 <jamesjacobthomas7@gmail.com>
2019-06-19 13:11:24 -0400

    created react boiler plate for Youtbe Browser mini app

---
+++ youtubebrowser/src/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>Youtube API browser</div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
