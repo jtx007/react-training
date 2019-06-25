325f050d07a1360a5c1005edbbd517b46f1b8a0d
jtx007 <jamesjacobthomas7@gmail.com>
2019-06-25 12:43:12 -0400

    implemented google oauth into application

---
+++ twitchclone/src/components/GoogleAuth.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+class GoogleAuth extends Component {
+
+    state = {
+        isSignedIn: null
+    }
+
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: `${process.env.REACT_APP_OAUTH_CLIENT_ID}`,
+                scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance()
+                this.setState({isSignedIn: this.auth.isSignedIn.get()})
+                this.auth.isSignedIn.listen(this.onAuthChange)
+            })
+        })
+    }
+
+    onAuthChange = () => {
+        this.setState({
+            isSignedIn: this.auth.isSignedIn.get()
+        })
+    }
+
+    onSignIn = () => {
+        this.auth.signIn()
+    }
+
+    onSignOut = () => {
+        this.auth.signOut()
+    }
+
+    renderAuthButton = () => {
+        if (this.state.isSignedIn === null) {
+            return null
+        } else if (this.state.isSignedIn) {
+            return (
+                <button onClick={this.onSignOut} className="ui red google button">
+                    <i className="google icon" />
+                    Sign Out
+                </button>
+            )
+        } else {
+            return (
+                <button onClick={this.onSignIn} className="ui primary google button">
+                    <i className="google icon" />
+                    Sign In with Google
+                </button>
+            )
+        }
+    }
+
+    render() {
+        return (
+            <div>{this.renderAuthButton()}</div>
+        )
+    }
+}
+
+export default GoogleAuth
