0bf4c8dd34c7e9598ad54c1d3247825a8abf37b2
jtx007 <jamesjacobthomas7@gmail.com>
2019-06-27 22:55:36 -0400

    added streaming capability

--- twitchclone/src/components/streams/StreamShow.js
+++ twitchclone/src/components/streams/StreamShow.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react'
+import flv from 'flv.js';
 import { connect } from 'react-redux'
 import { fetchStream } from '../../actions/index'
 
 class StreamShow extends Component {
+    constructor(props) {
+        super(props)
 
+        this.videoRef = React.createRef()
+    }
     componentDidMount() {
-        this.props.fetchStream(this.props.match.params.id)
+        const { id } = this.props.match.params
+        this.props.fetchStream(id)
+        this.buildPlayer()
+    }
+
+    componentDidUpdate() {
+        this.buildPlayer()
+    }
+
+    buildPlayer = () => {
+        if (this.player || !this.props.stream) {
+            return
+        }
+        const { id } = this.props.match.params
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        })
+        this.player.attachMediaElement(this.videoRef.current)
+        this.player.load()
     }
     
     render() {
         if (!this.props.stream) {
-            return <div>Loading...</div>
+            return <video ref={this.videoRef} style={{ width: "100%" }} controls/>
         }
+        const { title, description } = this.props.stream
         return (
             <div>
-                {this.props.stream.title}
+                <video ref={this.videoRef} style={{ width: "100%" }} controls/>
+                <h1>{title}</h1>
+                <h5>{description}</h5>
             </div>
         )
     }
